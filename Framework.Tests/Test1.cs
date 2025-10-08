06cd4814e2fe5db07c3f7e187dbec4487d608db6
walter <walter-eidi@hotmail.com>
2025-10-08 20:35:32 -0300

    adicionado novo fluxo

---
+++ Framework.Tests/Test1.cs
@@ -0,0 +1,17 @@
+﻿namespace Pipeline.Tests
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            // This method is called after each test method.
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
